3e18d39bf355d6bf2cb907d0acde5d6d8c555560
syb0rg <darksyb0rg@gmail.com>
2016-05-09 22:10:15 -0500

    Cleaned up PortAudio dependency code

--- cmake/FindPortAudio.cmake
+++ cmake/FindPortAudio.cmake
@@ -17,21 +17,6 @@ if (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
     # in cache already
     set(PORTAUDIO_FOUND TRUE)
 else (PORTAUDIO_LIBRARIES AND PORTAUDIO_INCLUDE_DIRS)
-    if (NOT WIN32)
-        include(FindPkgConfig)
-        pkg_check_modules(PORTAUDIO2 portaudio-2.0)
-    endif (NOT WIN32)
-
-if (PORTAUDIO2_FOUND)
-    set(PORTAUDIO_INCLUDE_DIRS ${PORTAUDIO2_INCLUDE_DIRS})
-    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-        set(PORTAUDIO_LIBRARIES "${PORTAUDIO2_LIBRARY_DIRS}/lib${PORTAUDIO2_LIBRARIES}.dylib")
-    else (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-        set(PORTAUDIO_LIBRARIES ${PORTAUDIO2_LIBRARIES})
-    endif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    set(PORTAUDIO_VERSION 19)
-    set(PORTAUDIO_FOUND TRUE)
-else (PORTAUDIO2_FOUND)
     find_path(PORTAUDIO_INCLUDE_DIR
     NAMES
     portaudio.h
@@ -80,8 +65,6 @@ else (PORTAUDIO2_FOUND)
             message(FATAL_ERROR "Could not find Portaudio")
         endif (Portaudio_FIND_REQUIRED)
     endif (PORTAUDIO_FOUND)
-endif (PORTAUDIO2_FOUND)
-
 
 # show the PORTAUDIO_INCLUDE_DIRS and PORTAUDIO_LIBRARIES variables only in the advanced view
 mark_as_advanced(PORTAUDIO_INCLUDE_DIRS PORTAUDIO_LIBRARIES)
